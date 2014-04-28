675f92a4e1b65b2eb15ab52a0d8dfe4e7acfe133
jrichelsen <jrichelsen@gmail.com>
2014-04-28 11:43:25 -0400

    Standardize Timer header and source files

--- Timer.cpp
+++ Timer.cpp
@@ -1,60 +1,64 @@
-/* Implementation of timer class. Class assists background scrolling functionality */
-
-#include "SDL/SDL.h"
-#include "SDL/SDL_image.h"
-#include <string>
-#include "Background.h"
+/*
+Bill Gowans, Jack Magiera, Jon Richelsen
+CSE20212
+1943_Project
+Timer.cpp
+	Implementation of Timer class (tracks time for framerate)
+History
+	04/11/14	Bill Gowans		Create, define default constructor, start(), stop(), pause(), unpause(), get_ticks(), is_started(), and is_paused()
+	04/28/14	Jon Richelsen	Standardize
+*/
 #include "Timer.h"
+#include "SDL/SDL.h"
 
 Timer::Timer() {
-  startTicks = 0;
-  pausedTicks = 0;
-  paused = false;
-  started = false;
+	paused = 0;
+	started = 0;
+	pausedTicks = 0;
+	startTicks = 0;
 }
 
 void Timer::start() {
-  started = true;
-  paused = false;
-  startTicks = SDL_GetTicks();
+	paused = 0;
+	started = 1;
+	startTicks = SDL_GetTicks();
 }
 
 void Timer::stop() {
-  started = false;
-  paused = false;
+	paused = 0;
+	started = 0;
 }
 
 void Timer::pause() {
-  if ((started == true)&&(paused == false)) {
-    paused = true;
-    pausedTicks = SDL_GetTicks() - startTicks;
-  }
+	if(started && !paused) {
+		paused = 1;
+		pausedTicks = SDL_GetTicks() - startTicks;
+	}
 }
 
 void Timer::unpause() {
-  if (paused == true) {
-    paused = false;
-    startTicks = SDL_GetTicks() - pausedTicks;
-    pausedTicks = 0;
-  }
+	if(paused) {
+		paused = 0;
+		startTicks = SDL_GetTicks() - pausedTicks;
+		pausedTicks = 0;
+	}
 }
 
 int Timer::get_ticks() {
-  if (started == true) {
-    if (paused == true) {
-      return pausedTicks;
-    }
-    else {
-      return SDL_GetTicks() - startTicks;
-    }
-  }
+	if(started) {
+		if(paused) {
+			return pausedTicks;
+		} else {
+			return SDL_GetTicks() - startTicks;
+		}
+	}
   return 0;
 }
 
-bool Timer::is_started() {
+int Timer::is_started() {
   return started;
 }
 
-bool Timer::is_paused() {
+int Timer::is_paused() {
   return paused;
 }
--- Timer.h
+++ Timer.h
@@ -1,25 +1,32 @@
-/* Class definition of timer class that assists with background scrolling functionality */
-
+/*
+Bill Gowans, Jack Magiera, Jon Richelsen
+CSE20212
+1943_Project
+Timer.h
+	Interface of Timer class (tracks time for framerate)
+History
+	04/11/14	Bill Gowans		Create, declare default constructor, start(), stop(), pause(), unpause(), get_ticks(), is_started(), is_paused(), and private variables startTicks, pausedTicks, paused, and started
+	04/28/14	Jon Richelsen	Standardize
+To Do
+*/
 #ifndef TIMER_H
 #define TIMER_H
 
-class Timer
-{
-    private:
-      int startTicks;
-      int pausedTicks;
-      bool paused;
-      bool started;
-
-    public:
-      Timer();
-      void start();
-      void stop();
-      void pause();
-      void unpause();
-      int get_ticks();
-      bool is_started();
-      bool is_paused();
+class Timer {
+	public:
+		Timer();
+		void start();
+		void stop();
+		void pause();
+		void unpause();
+		int get_ticks();
+		int is_started();
+		int is_paused();
+	private:
+		int paused;
+		int started;
+		int pausedTicks;
+		int startTicks;
 };
 
-#endif
+#endif //TIMER_H
